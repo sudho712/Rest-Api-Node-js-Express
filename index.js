a36630bcfd074789c5adf0575a54d35209d54db9
Sudhir Kumar <166948230+sudho712@users.noreply.github.com>
2024-11-23 21:40:49 +0530

    fetch all 1000 data MOCK_DATA.json

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const users = require('./MOCK_DATA.json');
 
 const app =express();
 const PORT=3000;
 
 // Routes
-app.listen(PORT, () => console.log('server started at PORT:${PORT}'));
+
+app.get("/users", (req, res) => {
+    return res.json(users);
+});
+
+app.listen(PORT, () => console.log(`server started at PORT:${PORT}`));
 
