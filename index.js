45454f9f75fd4c6d7091c71bb83943decd420dd2
Sudhir Kumar <166948230+sudho712@users.noreply.github.com>
2024-11-24 00:01:45 +0530

    fetch delte and pu data

--- index.js
+++ index.js
@@ -6,9 +6,58 @@ const PORT=3000;
 
 // Routes
 
-app.get("/users", (req, res) => {
+app.get("/api/users", (req, res) => {
     return res.json(users);
 });
+
+app.get("/users", (req,res)=>{
+const html=`
+<ul>
+    ${users.map((user)=> `<li>${user.first_name}</li>`)}
+</ul>
+`;
+res.send(html);
+});
+
+
+app
+.route("/api/users/:id")
+.get((req, res) => {
+    const id = Number(req.params.id); // Convert `id` to a number
+    const user = users.find(user => user.id === id); // Find user with matching ID
+    return res.json(user);
+}).patch((req,res) => {
+    // Edit with user id
+    return res.json({status: "pending"});
+})
+.delete((req,res)=>{
+    // delete with user id
+    return res.json({status: "pending"});
+
+});
+/* 
+app.get("/api/users/:id", (req, res) => {
+    const id = Number(req.params.id); // Convert `id` to a number
+    const user = users.find(user => user.id === id); // Find user with matching ID
+    return res.json(user);
+}); */
+
+/* // crete a new user
+app.post('/api/users', (req,res) => {
+    return res.json({status:"pending"});
+});
+
+
+// edit he user with id
+app.patch('/api/users/:id', (req,res) => {
+    return res.json({status:"pending"});
+});
+
+
+// delete he user with id
+app.delete('/api/users/:id', (req,res) => {
+    return res.json({status:"pending"});
+}); */
 
 app.listen(PORT, () => console.log(`server started at PORT:${PORT}`));
 
